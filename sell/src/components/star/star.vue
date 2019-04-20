06a872e4a1ccf44a15a14d506f4bae60c50b1c75
Joseph Jin <jerkjoe61@gmail.com>
2019-04-20 14:04:23 -0700

    star component

---
+++ sell/src/components/star/star.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="star" :class="starType">
+        <span v-for="itemClass in itemClasses" :class="itemClass" :key='itemClass' class="star-item">
+
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        size: {
+            type: Number
+        },
+        score: {
+            type: Number
+        },
+    },
+    computed: {
+        starType() {
+            return 'star-' + this.size
+        }
+    }
+}
+</script>
+
+<style lang="stylus" <style lang="stylus" scoped>
+.star
+    &.star-48
+    &.star-36
+    &.star-24
+    .star-item
+        display  inline-block
+        background-repeat no-repeat
+
+
+
+</style>
