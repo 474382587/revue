96ef975cd1735dedcfb3a834d00f6a0a7e5669fb
Joseph Jin <jerkjoe61@gmail.com>
2019-04-23 22:24:54 -0700

    better scroll

--- sell/src/components/products/Products.vue
+++ sell/src/components/products/Products.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
         <ul>
             <li v-for="(item,index) in goods" :key="index" class="menu-item">
                 <span class="text">
@@ -10,16 +10,47 @@
             </li>
         </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper" ref="foodsWrapper">
+        <ul>
+            <li v-for="(item, index) in goods" :key="index" class="food-list">
+                <h1 class="title"> {{item.name}} </h1>
+                <ul>
+                    <li v-for="(food, index) in item.foods" :key="index" class="food-item">
+                        <div class="icon">
+                            <img width="57px" height="57px" :src=food.icon alt="">
+                        </div>
+                        <div class="content">
+                            <h2 class="name">
+                                {{food.name}}
+                            </h2>
+                            <p class="desc">
+                                {{food.description}}
+                            </p>
+                            <div class="extra">
+                                <span class="count">
+                                    月售{{food.sellCount}}
+                                </span><span>好评率{{food.rating}}</span>
+                            </div>
+                            <div class="price">
+                                <span class="now">${{food.price}}</span>
+                                <span class="old" v-show="food.oldPrice">${{food.oldPrice}}</span>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import BScroll from 'better-scroll'
 export default {
     data() {
         return {
-            goods: [] 
+            goods: [],
         }
     },
     props: {
@@ -33,8 +64,18 @@ export default {
             if(res.status === 200) {
                 this.goods = res.data.goods
                 console.log(this.goods)
+                this.$nextTick(() => {
+                    this._initScroll()
+                })
             }
         })
+    },
+    methods: {
+        _initScroll() {
+            console.log(this.$refs)
+            this.meunScroll = new BScroll(this.$refs.menuWrapper, {});
+            this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {});
+        }
     }
 };
 </script>
@@ -83,4 +124,52 @@ export default {
                         bg-image('./invoice_3');
         .foods-wrapper
             flex 1
+            .title
+                padding-left 14px
+                height 26px
+                line-height 26px
+                border-left 2px solid #d9dde1
+                font-size 12px
+                color rgb(147, 153, 159)
+                background-color #f3f5f7
+            .food-item 
+                display flex
+                margin 18px
+                padding-bottom 18px
+                border-bottom 1px solid #d9dde1
+                &:last-child 
+                    border-bottom none
+                    margin-bottom 0
+                .icon
+                    flex 0 0 57px
+                    margin-right 10px
+                .content
+                    flex 1
+                    .name
+                        margin 2px 0 8px 0
+                        height 14px
+                        line-height 14px
+                        font-size 14px
+                        color rgb(7,17,27)
+                    .desc, .extra
+                        line-height 10px
+                        font-size 10px
+                        color rgb(147, 153, 159)
+                    .desc 
+                        margin-bottom 8px
+                        line-height 12px
+                    .extra
+                        .count 
+                            margin-right 12px
+                    .price
+                        font-weight 700
+                        line-height 24px
+                        .now 
+                            margin-right 8px
+                            color rgb(240, 20, 20)
+                            font-size: 14px
+                        .old
+                            text-decoration line-through
+                            font-size: 10px
+                            color: rgb(147, 153, 159)
 </style>
