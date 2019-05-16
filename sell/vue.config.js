a84a1842cc5e7f2cfe30765773b6f5dd615d0026
Joseph Jin <jerkjoe61@gmail.com>
2019-05-15 21:19:20 -0700

    build

--- sell/vue.config.js
+++ sell/vue.config.js
@@ -1,6 +1,9 @@
 // vue.config.js
 const path = require('path');
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/revue/sell/dist/'
+    : '/',
     configureWebpack: {
         resolve: {
             alias: {
